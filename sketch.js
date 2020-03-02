cb3ee5ab3dc9e50f3292e081e50f28b6b43b5cf1
joe10994 <joe10994@gmail.com>
2020-03-02 09:28:05 -0500

    Initial Commit

---
+++ sketch.js
@@ -0,0 +1,10 @@
+
+// setup is run once on startup and is generally used to "set up" the canvas and any other neccessary initial functions
+function setup() {
+  createCanvas(200, 200)
+}
+
+// draw is run on a continuous loop at a max frames per second of 60, and is generally used to create visuals within the canvas
+function draw() {
+background(62)
+}

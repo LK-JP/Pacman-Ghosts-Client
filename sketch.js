f5b9fbf85b1f2f82b21a4322eb3b2597acac811b
joe10994 <joe10994@gmail.com>
2020-03-24 16:02:25 -0400

    Begin Creation of Game Board
    - Create Wall Class
    - Center Canvas
    - Create top row of walls with 25 pixels between for path
    - Add position text on walls for debugging

--- sketch.js
+++ sketch.js
@@ -1,10 +1,34 @@
+Walls = []
+const createWalls = () => {
+  const scale = 10
+ 
+  // bottom border
+  Walls.push(new Wall(0, height - scale, width, scale, 1))
+  // obstacles
+  Walls.push(new Wall(36, 36, 85, 35))
+  Walls.push(new Wall(147, 36, 115, 35))
+  Walls.push(new Wall(288 , 0 ,24, 71 ))
+  Walls.push(new Wall(width-121,36, 85, 35))
+  Walls.push(new Wall(width - 121-26-115, 36, 115, 35))
 
-// setup is run once on startup and is generally used to "set up" the canvas and any other neccessary initial functions
+  //---------------------------------------
+   // border
+  // top border
+  Walls.push(new Wall(0, 0, width, scale, 1))
+  // left border
+  Walls.push(new Wall(0, 0, scale, height, 1))
+  // right border
+  Walls.push(new Wall(width - scale, 0, scale, height, 1))
+}
+
+// setup is run once on startup and is generally used to "set up" the canvas and any other necessary initial functions
 function setup() {
-  createCanvas(200, 200)
+  createCanvas(600, 700)
+  createWalls()
 }
 
 // draw is run on a continuous loop at a max frames per second of 60, and is generally used to create visuals within the canvas
 function draw() {
-background(62)
+  background(62)
+  Walls.forEach(wall => wall.show())
 }

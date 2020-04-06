04ede5dd11c0f50e6d1729df880a7846bae62299
joe10994 <joe10994@gmail.com>
2020-04-06 16:30:24 -0400

    Add ability to start new game
    - Added reset buttton
    - Extracted draw views into global functions
    - Add restart function to game class
    -

--- sketch.js
+++ sketch.js
@@ -1,14 +1,15 @@
 let game
 let pacman
 let startButton
-const cols = 23
-const rows = 27
+let resetButton
+let cols
+let rows
 const scale = 25
 
 let pacmanImg
 let pacmanFrames = []
 
-function windowResized(){
+function windowResized() {
   startButton.center()
 }
 
@@ -22,12 +23,13 @@ function preload() {
 
 // setup is run once on startup and is generally used to "set up" the canvas and any other necessary initial functions
 function setup() {
-
   createCanvas(575, 675)
+  cols = width / scale
+  rows = height / scale
   //frameRate changed for easier animations and movement speed changes
   frameRate(30)
 
-   // animation setup 
+  // animation setup
   animationSetUp()
 
   //create new game
@@ -35,23 +37,26 @@ function setup() {
 
   // game board set up
   game.setupGrid()
-  game.grid[10][5].dot = true
- 
+
   //create new pacman
-  pacman = new Pacman(game.grid)
+  pacman = new Pacman(game.grid, scale)
 
+  // get the button
 
+  startButton = createButton('Start Game').size(100, 35).center()
+  // startButton.position(windowWidth/2-50, windowHeight/2)
+  startButton.mousePressed(() => game.start())
 
-    // get the button
-
-    startButton = createButton('Start Game').size(100,35).center()
-    // startButton.position(windowWidth/2-50, windowHeight/2)
-    startButton.mousePressed( () => game.start())
-    
+  resetButton = createButton('Restart Game').size(100, 35).center()
+  // resetButton.position(windowWidth/2-50, windowHeight/2)
+  resetButton.mousePressed(() => {
+    game.restart()
+    pacman = new Pacman(game.grid, scale)
+  })
+  resetButton.hide()
 }
 
-
-
+// creates sprite array for pacman
 function animationSetUp() {
   for (let i = 0; i < 5; i++) {
     for (let j = 0; j < 4; j++) {
@@ -64,7 +69,44 @@ function animationSetUp() {
 // draw is run on a continuous loop at a max frames per second of 60, and is generally used to create visuals within the canvas
 function draw() {
   background(62)
-  if(game.playing){
+  if (game.won) {
+    showResetScreen()
+  } else if (game.playing) {
+    showPlayGame()
+  } else if (!game.gameOver) {
+    showStartScreen()
+  } else if (game.gameOver){
+    showGameOver()
+  }
+}
+
+function showGameOver(){
+    resetButton.show()
+    background(0)
+    push()
+    textAlign(CENTER)
+    textSize(16)
+    fill(255)
+    text(
+      `You lost! Final score was ${game.score}. Click restart to play again`,
+      width / 2,
+      height / 3
+    )
+    pop()
+}
+
+function showStartScreen() {
+  startButton.show()
+  background(0)
+  push()
+  textAlign(CENTER)
+  textSize(30)
+  fill(255)
+  text(`Click Start to begin a new game`, width / 2, height / 3)
+  pop()
+}
+
+function showPlayGame() {
   game.show()
   pacman.show()
   pacman.tryToChangeDir()
@@ -74,15 +116,22 @@ function draw() {
   game.finishGame()
   fill(255)
   text(`Score: ${game.score}`, 10, 10)
+  text(`Lives: ${game.lives}`, width - 100, 10)
+  resetButton.hide()
   startButton.hide()
-}else {
-  startButton.show()
+}
+
+function showResetScreen() {
+  resetButton.show()
   background(0)
   push()
   textAlign(CENTER)
-  textSize(30)
+  textSize(16)
   fill(255)
-  text(`Click Start to begin a new game`,width/2, height/3)
+  text(
+    `You won! Final score was ${game.score} and you had ${game.lives} lives left. Click restart to play again`,
+    width / 2,
+    height / 3
+  )
   pop()
-}
 }
